65c6850fe5a2b223094b31e407d43bab80212c52
william austin <william.austin@solutiondesign.com>
2014-08-05 12:45:55 -0500

    initial commit

---
+++ LongPoll/Controllers/ChatController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using LongPoll.Models;
+
+
+namespace LongPoll.Controllers
+{
+    public class ChatController : Controller
+    {
+
+
+         static TaskCompletionSource<string> _nextMessage = new TaskCompletionSource<string>();
+
+        //
+        // GET: /Chat/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        [HttpGet]
+        public async Task<string> LongPoll()
+        {
+            string theMessage =  await _nextMessage.Task;
+
+            return theMessage;
+        }
+
+        [HttpPost]
+        public void PostMessage(string message)
+        {
+            _nextMessage.SetResult(message);
+            _nextMessage = new TaskCompletionSource<string>();
+
+        }
+
+    }
+}
---
+++ LongPoll/Models/ChatViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LongPoll.Models
+{
+    public class ChatViewModel
+    {
+        public string Chat { get; set; }
+    }
+}
